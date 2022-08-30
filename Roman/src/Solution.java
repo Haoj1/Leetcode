4b8931b3e445416a4913869b5d52374847bc446e
Haoj1 <99279909+Haoj1@users.noreply.github.com>
2022-08-30 15:46:51 -0500

    rotate image 08/30

--- Roman/src/Solution.java
+++ Roman/src/Solution.java
@@ -1,15 +1,47 @@
 
+import java.util.*;
+
 public class Solution {
   
+  
   public int romanToInt(String s) {
-    return 3;
+    HashMap<Character, Integer> roman = new HashMap<>();
+    int res = 0;
+    int counts = 0;
+    roman.put('I', 1);
+    roman.put('V', 5);
+    roman.put('X', 10);
+    roman.put('L', 50);
+    roman.put('C', 100);
+    roman.put('D', 500);
+    roman.put('M', 1000);
+    for (int i = 0; i < s.length(); i++) {
+      counts++;
+      if (i+1 < s.length()) {
+        if (roman.get(s.charAt(i)) == roman.get(s.charAt(i+1))) {
+          
+        }
+        else if (roman.get(s.charAt(i)) > roman.get(s.charAt(i+1))){
+          res += counts * roman.get(s.charAt(i));
+          counts = 0;
+        }
+        else {
+          res -= counts * roman.get(s.charAt(i));
+          counts = 0;
+        }
+      }
+      else {
+        res += counts * roman.get(s.charAt(i));
+      }
+    }
+    return res;
 
   }
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub\
     Solution sol = new Solution();
-    String s = "X";
+    String s = "MCMXCIV";
     System.out.println(sol.romanToInt(s));
   }
 
